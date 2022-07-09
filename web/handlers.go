a26790c9a995dd088a9f8bdbf69160f5a67fa182
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-07-09 20:55:32 +0430

    set template for showing articles

--- web/handlers.go
+++ web/handlers.go
@@ -1,9 +1,11 @@
 package web
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 
+	"github.com/Mohammad-Hakemi22/tmoh/db"
 	"github.com/gorilla/mux"
 )
 
@@ -15,7 +17,14 @@ func Router() *mux.Router {
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "hi")
+	tpl := template.Must(template.ParseFiles("D:/Go/TMOH/templates/home.html"))
+	articles := []db.Article{}
+	articles = append(articles, db.Article{Title: "hi", Text: "hello world", Date: time.Now(), Rate: 2.6, Athor: &db.Athor{Name: "mmd", Bio: "hi there", Age: 25}})
+	articles = append(articles, db.Article{Title: "khabar", Text: "salam !", Date: time.Now(), Rate: 3.7, Athor: &db.Athor{Name: "ali", Bio: "salam", Age: 35}})
+	tpl.Execute(w, articles)
+	// if err != nil {
+	// 	http.Error(w, "Can't execute template", http.StatusInternalServerError)
+	// 	return
+	// }
 
 }
