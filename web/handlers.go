7dd90596125ffc33ea508d24e586d0c25f1074f0
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-07-16 21:03:32 +0430

    add update handler

--- web/handlers.go
+++ web/handlers.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"html/template"
 	"math/rand"
 	"net/http"
@@ -18,18 +19,13 @@ func Router() *mux.Router {
 	r.HandleFunc("/", HomePage).Methods("GET")
 	r.HandleFunc("/form", FormArticle).Methods("GET")
 	r.HandleFunc("/create", CreateArticle).Methods("POST")
-
+	r.HandleFunc("/update/{id}", UpdateArticle).Methods("GET")
+	r.HandleFunc("/update", Update).Methods("POST")
 	return r
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	// wd, err := os.Getwd()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 	tpl := template.Must(template.ParseFiles("D:/Go/TMOH/templates/home.html"))
-	// articles = append(articles, db.Article{Title: "hi", Text: "hello world", Date: time.Now(), Rate: 2.6, Athor: &db.Athor{Name: "mmd", Bio: "hi there", Age: 25}})
-	// articles = append(articles, db.Article{Title: "khabar", Text: "salam !", Date: time.Now(), Rate: 3.7, Athor: &db.Athor{Name: "ali", Bio: "salam", Age: 35}})
 	err := tpl.Execute(w, articles)
 	if err != nil {
 		http.Error(w, "Can't execute template", http.StatusInternalServerError)
@@ -59,4 +55,28 @@ func CreateArticle(w http.ResponseWriter, r *http.Request) {
 	bio := r.FormValue("AuthorBio")
 	age, _ := strconv.Atoi(r.FormValue("AuthorAge"))
 	articles = append(articles, db.Article{Id: id, Title: title, Text: text, Date: date, Rate: rate, Athor: &db.Athor{Name: name, Bio: bio, Age: age}})
+	fmt.Println(articles)
+}
+
+func UpdateArticle(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("D:/Go/TMOH/templates/update.html"))
+	params := mux.Vars(r)
+	for idx, a := range articles {
+		if id, _ := strconv.Atoi(params["id"]); id == a.Id {
+			tpl.Execute(w, a)
+			articles = append(articles[:idx], articles[idx+1:]...)
+		}
+	}
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	defer http.Redirect(w, r, "/", http.StatusSeeOther)
+	title := r.FormValue("title")
+	text := r.FormValue("text")
+	date := time.Now().Format("01-02-2006 Monday")
+	rate, _ := strconv.ParseFloat(r.FormValue("rate"), 32)
+	name := r.FormValue("AuthorName")
+	bio := r.FormValue("AuthorBio")
+	age, _ := strconv.Atoi(r.FormValue("AuthorAge"))
+	articles = append(articles, db.Article{Title: title, Text: text, Date: date, Rate: rate, Athor: &db.Athor{Name: name, Bio: bio, Age: age}})
 }
