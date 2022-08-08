00a08a63d5dd2c6414103383cd33d99cfddf9d2a
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-08-08 22:39:03 +0430

    change route package

--- web/server.go
+++ web/server.go
@@ -3,10 +3,12 @@ package web
 import (
 	"log"
 	"net/http"
+
+	"github.com/Mohammad-Hakemi22/tmoh/routes"
 )
 
 func RunServer() {
-	r := Router()
+	r := routes.Router()
 
 	log.Fatalln(http.ListenAndServe(":8080", r))
 }
