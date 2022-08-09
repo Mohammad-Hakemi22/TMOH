cab8c4bf854be63fc4545c775a62da0d58c28eaa
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-08-09 22:27:15 +0430

    init server and database connection

--- cmd/tmoh/main.go
+++ cmd/tmoh/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/Mohammad-Hakemi22/tmoh/config"
 	"github.com/Mohammad-Hakemi22/tmoh/web"
 )
+
+func init() {
+	config.SetConfig()
+}
 
 func main() {
 	fmt.Println("Hello to the Earth")
