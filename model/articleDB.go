d0c16f4be81fb44eca18c2341c268e6715d8febe
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-08-19 21:43:29 +0430

    set foreign key to user

--- model/articleDB.go
+++ model/articleDB.go
@@ -1,11 +1,12 @@
 package model
 
+
 type Article struct {
-	Id    int
-	Title string
-	Text  string
-	Date  string
-	Rate  float64
-	Athor *Athor
-	VIP   bool
+	Id       int     `gorm:"primaryKey"`
+	Title    string  `json:"title"`
+	Text     string  `json:"text"`
+	Date     string  `json:"date"`
+	Rate     float64 `json:"rate"`
+	Vip      bool    `json:"vip"`
+	AuthorID int     `gorm:"foreignkey"`
 }
